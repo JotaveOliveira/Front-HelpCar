e69c9122868949426ca9cf0535fa413ab0c9521e
Joao Vitor Oliveira de Almeida <jvitoroliveira@brq.com>
2020-02-12 11:37:29 -0300

    Subindo alguns components

---
+++ src/components/LoginUsuario.vue
@@ -0,0 +1,66 @@
+<template>
+     <form class="form-signin">
+        <img class="mb-4" src="Images/IconLogin.png" alt="icon" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+        <label for="inputEmail" class="sr-only">Email</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+        <label for="inputPassword" class="sr-only">Senha</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+        <div class="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me"> Lembrar de mim
+          </label>
+        </div>
+        <button class="btn btn-lg btn-danger btn-block" type="submit">Entrar</button>
+        <p class="mt-5 mb-3 text-muted">&copy; HelpCar 2020</p>
+      </form>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+.form-signin {
+  height: 100%;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #fff;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
