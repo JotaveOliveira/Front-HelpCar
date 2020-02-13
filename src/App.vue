aa7743aaa1e78752bc452ac3605e2387829b8100
Joao Vitor Oliveira de Almeida <jvitoroliveira@brq.com>
2020-02-13 13:31:57 -0300

    Deletando o store e mudando coida no app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
+
     <div id="nav">
       <router-link to="/">Home</router-link>
       <router-link to="/login">LoginUsuario</router-link>
       <router-link to="/usuarios">CadastroUsuario</router-link>
     </div>
+
+    <router-view/>
+    
   </div>
 
 </template>
