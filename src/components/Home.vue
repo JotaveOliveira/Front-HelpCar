ba53003850794b1b6f0d730c8f58f153ffe30687
Joao Vitor Oliveira de Almeida <jvitoroliveira@brq.com>
2020-02-27 11:36:45 -0300

    Subindo o home com scroll na page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -34,51 +34,58 @@
         </header>
 
         <main role="main">
-
-            <div class="container marketing">
-                <br />
-                <br />
-                <br />
-                <div class="row featurette">
-                    <div class="col-md-7">
-                        <h2 class="featurette-heading">Sobre a <span class="text-muted">HelpCar</span></h2>
-                        <p class="lead"> HelpCar busca agilizar e facilitar o seu dia a dia com seu carro, estamos
-                            acostumados a ir em oficinas
-                            para manutenção e reparos e nos deparar com gigantes filas de espera para ser atendidos,
-                            isso
-                            está
-                            com dias contados !! A HelpCar vem para te ajudar a escolher o melhor prestador do serviço,
-                            mais
-                            próximo de você e o mais importante disposto para te atender na HORA!! Não é incrivel ??
-                            Você
-                            poderá ser socorrido em qualquer hora e qualquer lugar, ficando fora de acostamento por
-                            horas a
-                            espera de socorro!!! Faça seu cadastro já !! É simples, facil, rapido e de GRACHA!! Ops de
-                            Graça!!!!</p>
-                    </div>
-                    <div class="col-md-5">
-                        <img class="d-block w-100" src="../assets/imagemSobre.jpg" alt="imagem1" />
+            <div class="anime">
+                <div class="container marketing">
+                    <br />
+                    <br />
+                    <br />
+                    <div class="row featurette">
+                        <div class="col-md-7">
+                            <h2 class="featurette-heading">Sobre a <span class="text-muted">HelpCar</span></h2>
+                            <p class="lead"> HelpCar busca agilizar e facilitar o seu dia a dia com seu carro, estamos
+                                acostumados a ir em oficinas
+                                para manutenção e reparos e nos deparar com gigantes filas de espera para ser atendidos,
+                                isso
+                                está
+                                com dias contados !! A HelpCar vem para te ajudar a escolher o melhor prestador do
+                                serviço,
+                                mais
+                                próximo de você e o mais importante disposto para te atender na HORA!! Não é incrivel ??
+                                Você
+                                poderá ser socorrido em qualquer hora e qualquer lugar, ficando fora de acostamento por
+                                horas a
+                                espera de socorro!!! Faça seu cadastro já !! É simples, facil, rapido e de GRACHA!! Ops
+                                de
+                                Graça!!!!</p>
+                        </div>
+                        <div class="col-md-5">
+                            <img class="d-block w-100" src="../assets/imagemSobre.jpg" alt="imagem1" />
+                        </div>
                     </div>
                 </div>
+            </div>
 
-                <hr class="featurette-divider">
-
+            <hr class="featurette-divider">
+            <div class="anime">
                 <div class="row featurette">
                     <div class="col-md-7 order-md-2">
                         <h2 class="featurette-heading">Quem é <span class="text-muted">HelpCar?</span></h2>
                         <p class="lead">HelpCar é uma empresa criada a partir de funcionários, desenvolvedores de
                             softwares,
-                            foi idealizada a partir de uma situação no cotidiano, a empresa busca sempre trazer a melhor
-                            aplicação para seu usuário, tentando agilizar seu processo do cotidiano e resolver situações
+                            foi idealizada a partir de uma situação no cotidiano, a empresa busca sempre trazer a
+                            melhor
+                            aplicação para seu usuário, tentando agilizar seu processo do cotidiano e resolver
+                            situações
                             constrangedoras. </p>
                     </div>
                     <div class="col-md-5 order-md-1">
                         <img class="d-block w-100" src="../assets/imagemQueme.jpg" alt="imagem2" />
                     </div>
                 </div>
+            </div>
 
-                <hr class="featurette-divider">
-
+            <hr class="featurette-divider">
+            <div class="anime">
                 <div class="row featurette">
                     <div class="col-md-7">
                         <h2 class="featurette-heading">Contatos <span class="text-muted">HelpCar</span></h2>
@@ -93,21 +100,26 @@
                         </iframe>
                     </div>
                 </div>
+            </div>
 
-                <hr class="featurette-divider">
-
+            <hr class="featurette-divider">
+            <div class="anime">
                 <div class="row featurette">
                     <div class="col-md-7 order-md-2">
-                        <h2 class="featurette-heading">Você é<span class="text-muted">Prestador de Serviço?</span></h2>
-                        <p class="lead">Se você tem seu próprio negócio e deseja participar do HelpCar para facilitar a
+                        <h2 class="featurette-heading">Você é<span class="text-muted">Prestador de Serviço?</span>
+                        </h2>
+                        <p class="lead">Se você tem seu próprio negócio e deseja participar do HelpCar para
+                            facilitar a
                             vida de muitas pessoas
-                            com o melhor atendimento e total segurança ao cliente e fazer o seu próprio negócio decolar
+                            com o melhor atendimento e total segurança ao cliente e fazer o seu próprio negócio
+                            decolar
                             com clientes de todos os lugares
                             cadastre-se clicando no link a seguir e faça parte do teste dessa aplicação <router-link
                                 to="/prestadoresServico">..Cadastre-se como prestador de serviço</router-link>
                         </p>
-                        
-                        <p>Já é cadastrado ? Deseja fazer login? </p><button class="btn btn-info my-2 my-sm-0" type="submit">
+
+                        <p>Já é cadastrado ? Deseja fazer login? </p><button class="btn btn-info my-2 my-sm-0"
+                            type="submit">
                             <router-link to="/loginPrestador" id="colorButton">Logar como prestador</router-link>
                         </button>
                     </div>
@@ -115,12 +127,10 @@
                         <img class="d-block w-100" src="../assets/imagemQueme.jpg" alt="imagem2" />
                     </div>
                 </div>
-
-
-                <hr class="featurette-divider">
-
-
             </div>
+
+
+            <hr class="featurette-divider">
 
             <footer class="container">
                 <p>&copy; 2020 HelpCar </p>
@@ -134,6 +144,48 @@
         name: 'Home'
     }
 </script>
+<script>
+    debounce = function (func, wait, immediate) {
+        var timeout;
+        return function () {
+            var context = this,
+                args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+
+    (function () {
+        var $target = $('.anime'),
+            animationClass = 'anime-start',
+            offset = $(window).height() * 3 / 4;
+
+        function animeScroll() {
+            var documentTop = $(document).scrollTop();
+
+            $target.each(function () {
+                var itemTop = $(this).offset().top;
+                if (documentTop > itemTop - offset) {
+                    $(this).addClass(animationClass);
+                } else {
+                    $(this).removeClass(animationClass);
+                }
+            })
+        }
+
+        animeScroll();
+        $(document).scroll(debounce(function () {
+            animeScroll();
+        }, 200));
+    }());
+</script>
+<script type="text/javascript" src="../jquery-3.1.1.min.js"></script>
 
 <style>
     a:hover {
